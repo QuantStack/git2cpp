01b269b67043c3a9cf22793d6640f22f96be9ed1
Sandrine Pataut <pataut.sandrine@gmail.com>
2025-06-25 16:28:19 +0200

    add branch info and flag to status subcommand

---
+++ src/wrapper/refs_wrapper.cpp
@@ -0,0 +1,17 @@
+#include "../utils/git_exception.hpp"
+#include "../wrapper/refs_wrapper.hpp"
+
+
+reference_wrapper::~reference_wrapper()
+{
+    git_reference_free(p_resource);
+    p_resource=nullptr;
+}
+
+const char* reference_wrapper::get_ref_name(const repository_wrapper& rw)
+{
+    reference_wrapper ref;
+    throwIfError(git_repository_head(&(ref.p_resource), rw));
+    const char* ref_name = git_reference_shorthand(ref.p_resource);
+    return ref_name;
+}
