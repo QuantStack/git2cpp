b4326ffe23816338dd0d35d87da6720258f2cadb
Sandrine Pataut <pataut.sandrine@gmail.com>
2026-04-09 16:37:13 +0200

    Add show-ref subcommand

---
+++ src/subcommand/showref_subcommand.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <CLI/CLI.hpp>
+
+#include "../utils/common.hpp"
+
+class showref_subcommand
+{
+public:
+
+    explicit showref_subcommand(const libgit2_object&, CLI::App& app);
+    void run();
+
+private:
+
+
+};
